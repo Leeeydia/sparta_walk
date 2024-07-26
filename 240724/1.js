19b3ce25a9c16e3681473bc3027e04ff3984a03f
leeeydia <leeeydia@gmail.com>
2024-07-26 14:27:06 +0900

    change the folder structure

---
+++ 240724/1.js
@@ -0,0 +1,4 @@
+const greeting = "Hello";
+const name = "World";
+const message = greeting.concat(" ", name);
+console.log(message) // "Hello World" 출럭
---
+++ 240726/3.js
@@ -0,0 +1,14 @@
+function repeatWord(str, n) {
+  for(let i = 0; i < n ; i++){
+    console.log(str)
+    }
+    }
+  ff
+
+
+// 함수 실행
+
+
+repeatWord("hello", 5);
+// ㅎ
+
